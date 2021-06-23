01e0ec1aa2f2b966c0b0256d21a6ac3a37f16513
Daniel Tran <trandaniel53@gmail.com>
2021-06-22 21:20:03 -0400

    Fix resume button

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,11 +6,11 @@ import './Page.css';
 
 
 function Page() {
-  function download() {
+  function download(fileUrl, fileName) {
     var a = document.createElement('a');
-    a.href = "/resources/resume.pdf";
+    a.href = fileUrl;
     a.target = '_blank';
-    a.download = "DanielTran.pdf";
+    a.download = fileName;
     a.click();
   }
 
@@ -20,7 +20,7 @@ function Page() {
       <div className="right">
         <h4>This page is currently under construction. Please check back soon for updates 👋</h4>
         <div className="buttonContainer">
-          <Button onClick={() => {download()}} variant="contained" color="primary">Resume</Button>
+          <Button onClick={() => {download("./DanielTran4Av4.pdf", "DanielTranResumsse.pdf")}} variant="contained" color="primary">Resume</Button>
         </div>
       </div>
       <span className="dot first"></span>
