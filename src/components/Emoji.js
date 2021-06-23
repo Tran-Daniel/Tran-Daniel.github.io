3b1f26c8d7e7937d98d7e731a1277b67d2be9c48
Daniel Tran <trandaniel53@gmail.com>
2021-06-22 21:01:05 -0400

    Add resume and functionality to Resume button

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -3,7 +3,7 @@ const Emoji = props => (
     <span
         className="emoji"
         role="img"
-        aria-label={props.label ? props.label : ""}
+        aria-label={props.label || ""}
         aria-hidden={props.label ? "false" : "true"}
     >
         {props.symbol}
--- src/components/Page.js
+++ src/components/Page.js
@@ -3,18 +3,28 @@ import Header from "./Header";
 import Button from '@material-ui/core/Button';
 import './Page.css';
 
+
+
 function Page() {
+  function download() {
+    var a = document.createElement('a');
+    a.href = "/resources/resume.pdf";
+    a.target = '_blank';
+    a.download = "DanielTran.pdf";
+    a.click();
+  }
+
   return (
     <div className="pageContainer">
       <Header></Header>
-      <span class="dot first"></span>
-      <span class="dot second"></span>
       <div className="right">
         <h4>This page is currently under construction. Please check back soon for updates 👋</h4>
-        <div class="buttonContainer">
-          <Button variant="contained" color="primary">Resume</Button>
+        <div className="buttonContainer">
+          <Button onClick={() => {download()}} variant="contained" color="primary">Resume</Button>
         </div>
       </div>
+      <span className="dot first"></span>
+      <span className="dot second"></span>
     </div>
   );
 }
