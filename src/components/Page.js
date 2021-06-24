45f00792ed96508677aec05e256d7593d7a501e4
Daniel Tran <trandaniel53@gmail.com>
2021-06-24 00:20:31 -0400

    Restructure classes, responsive design v0.1

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,26 +1,44 @@
 import React from "react";
-import Header from "./Header";
-import Button from '@material-ui/core/Button';
-import './Page.css';
-
-
+import Button from "@material-ui/core/Button";
+import "./Page.css";
 
 function Page() {
   function download(fileUrl, fileName) {
-    var a = document.createElement('a');
+    var a = document.createElement("a");
     a.href = fileUrl;
-    a.target = '_blank';
+    a.target = "_blank";
     a.download = fileName;
     a.click();
   }
 
   return (
-    <div className="pageContainer">
-      <Header></Header>
-      <div className="right">
-        <h4>This page is currently under construction. Please check back soon for updates 👋</h4>
-        <div className="buttonContainer">
-          <Button onClick={() => {download("./DanielTran4Av4.pdf", "DanielTranResume.pdf")}} variant="contained" color="primary">Resume</Button>
+    <div>
+      <div className="split">
+        <div className="container left">
+          <h1>DANIEL TRAN</h1>
+          <h2>SOFTWARE DEVELOPER</h2>
+          <h3>
+            4A CS Student at the University of Waterloo looking for a Fall 2021
+            Coop position.
+          </h3>
+          {/* <Link to="/second">Second</Link> */}
+        </div>
+        <div className="container right">
+          <h4>
+            This page is currently under construction. Please check back soon for
+            updates 👋
+          </h4>
+          <div className="buttonContainer">
+            <Button
+              onClick={() => {
+                download("./DanielTran4Av4.pdf", "DanielTranResume.pdf");
+              }}
+              variant="contained"
+              color="primary"
+            >
+              Resume
+            </Button>
+          </div>
         </div>
       </div>
       <span className="dot first"></span>
@@ -30,5 +48,3 @@ function Page() {
 }
 
 export default Page;
-
-
